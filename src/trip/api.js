7e622162167c7704eeec4409446ad73354a2a0e8
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-12-12 16:40:14 +0300

    Add createTrip API Call

--- src/trip/api.js
+++ src/trip/api.js
@@ -1,12 +1,24 @@
 import apiUrl from "../apiConfig";
 import axios from "axios";
 
+// Get All Trips
 export const allTrips = () => {
   return axios({
     method: "GET",
-    url: apiUrl + "/trips",
+    url: `${apiUrl}/trips`
     // headers: {
     //   Authorization: `Bearer ${user.token}` // FOR EXPRESS
     // }
+  });
+};
+
+// Create a Trip
+export const createTrip = (trip) => {
+  return axios({
+    method: "POST",
+    url: `${apiUrl}/trips`,
+    data: {
+      trip
+    }
   });
 }
