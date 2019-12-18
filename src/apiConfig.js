bbe4c394a037b051399e3bf6214aa7008be0ff02
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-12-18 17:04:06 +0300

    Add Files for Deployment

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl;
 const expressPort = 3000;
 const apiUrls = {
-  production: "https://aqueous-atoll-85096.herokuapp.com",
+  production: "http://fierce-sea-75816.herokuapp.com/",
   development: `http://127.0.0.1:${expressPort}`
 };
 
